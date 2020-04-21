53e00e283b26268ee211a5d5ccc2b5ba124f73c1
GameJutsu <GameJutsu@users.noreply.github.com>
2020-04-21 22:10:44 +0530

    Changed event to input from keypress

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var charNo = 0;
 var content;
 var current = "";
 var story = "Mera naam hi shinchan hai.... mai shararat se bhara!!! Badi mushkil me padi..... meri family Nohara!!!";
-$("textarea").keypress(function()
+
+$("textarea").on("input", function()
 {
 	$(this).val("");
 	$(this).val(current + story.charAt(charNo));
